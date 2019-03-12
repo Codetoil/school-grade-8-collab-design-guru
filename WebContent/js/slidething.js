6943dfbe5ef7c3d58bed5b3fb9e44c59e1b78134
Littoil <ianthisawesomee@gmail.com>
2019-03-12 13:42:31 -0400

    added support to edge and possibly more

--- WebContent/js/slidething.js
+++ WebContent/js/slidething.js
@@ -1,12 +1,11 @@
-/**
- * 
- */
 
 var slidedata;
 var rootslidegroup;
 var slidegroup;
 var slideNo = 1;
 var slideCount = 1;
+var includeLit; //true: "lit:(tagname)"; false: "(tagname)"
+var initialized = false;
 
 function Prev() {
 	if (slideNo > 1)
@@ -25,7 +24,7 @@ function Next() {
 }
 
 function postInit() {
-	//No reason for this to exist
+	initialized = true;
 	console.log("Finished PostInitialization Stage!");
 }
 
@@ -34,8 +33,18 @@ function postInit() {
 function UpdateSlideData()
 {
 	var data;
-	slideCount = slidegroup.getElementsByTagName("lit:slide").length;
-	data = slidegroup.children["slide"+slideNo].getElementsByTagName("lit:data")[0].textContent;
+	if (includeLit)
+	{
+		slideCount = slidegroup.getElementsByTagName("lit:slide").length - 1;
+		data = slidegroup.children["slide"+slideNo].getElementsByTagName("lit:data")[0].textContent;
+	}
+	else
+	{
+		slideCount = slidegroup.getElementsByTagName("slide").length - 1;
+		data = slidegroup.children["slide"+slideNo].getElementsByTagName("data")[0].textContent;
+	}
+	
+	
 	document.getElementById("main").innerHTML = data;
 	if (slideNo === 1)
 	{
@@ -76,29 +85,53 @@ function preInit() {
 function Init(data) {
 	slidedata = data;
 	console.log("globalized slide data!");
-	if (slidedata.getElementsByTagName("lit:root_slide_group").length == 0)
+	if (slidedata.getElementsByTagName("lit:root_slide_group").length > 0)
 	{
-		console.error("No found root_slide_group! the tag name is \"lit:root_slide_group\"!");
-		return;
+		includeLit = true;
+	} else if (slidedata.getElementsByTagName("root_slide_group").length > 0)
+	{
+		includeLit = false;
 	}
-	if (slidedata.getElementsByTagName("lit:root_slide_group").length > 1)
+	
+	if (slidedata.getElementsByTagName("lit:root_slide_group").length > 1 || slidedata.getElementsByTagName("root_slide_group").length > 1)
 	{
 		console.error("Too many root_slide_group(s)! Combine all the root_slide_group(s) into one!");
 		return;
 	}
-	rootslidegroup = slidedata.getElementsByTagName("lit:root_slide_group")[0];
+	
+	if (slidedata.getElementsByTagName("lit:root_slide_group").length == 0 && slidedata.getElementsByTagName("root_slide_group").length == 0)
+	{
+		console.error("No root_slide_groups! Use the tag name \"lit:root_slide_group\" in the xml!")
+	}
+	
+	if (includeLit)
+	{
+		rootslidegroup = slidedata.getElementsByTagName("lit:root_slide_group")[0];
+	}
+	else
+	{
+		rootslidegroup = slidedata.getElementsByTagName("root_slide_group")[0];
+	}
+	
 	console.log(rootslidegroup);
 	var children = rootslidegroup.children;
 	
 	console.log(children);
 	for (var i = 0; i < children.length; i++)
 	{
-		if (children[i].tagName === "lit:slide_group")
+		if ((children[i].tagName === "lit:slide_group") || (children[i].tagName === "slide_group"))
 		{
 			console.log(children[i]);
 		}
 	}
-	slidegroup = slidedata.getElementsByTagName("lit:root_slide_group")[0].children["init"];
+	if (includeLit)
+	{
+		slidegroup = slidedata.getElementsByTagName("lit:root_slide_group")[0].children["init"];
+	}
+	else
+	{
+		slidegroup = slidedata.getElementsByTagName("root_slide_group")[0].children["init"];
+	}
 	UpdateSlideData();
 	console.log("Finished Initialization Stage!");
 	console.log("Starting PostInitialization Stage!");
